90972b8d4bdc4610bbc523b149f1c78c0e69a8c1
sndjy1986 <sndjy1986@gmail.com>
2025-05-19 20:19:14 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -98,4 +98,5 @@ def make_destination(truck_id):
 
 if __name__ == "__main__":
     load_config()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
